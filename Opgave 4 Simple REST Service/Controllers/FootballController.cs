ef80c308042988b0d22c2e34649968cd35731b36
Victor Dahl Saltoft <vict2729@edu.easj.dk>
2021-10-05 14:24:52 +0200

    Add project files.

---
+++ Opgave 4 Simple REST Service/Controllers/FootballController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Opgave_1_Csharp;
+using Opgave_4_Simple_REST_Service.Mangers;
+
+namespace Opgave_4_Simple_REST_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballController : ControllerBase
+    {
+        private FootballplayerManger footballplayerManger = new FootballplayerManger();
+
+        [HttpGet]
+        public IEnumerable<FootballPlayer> GetAll([FromQuery] string substring)
+        {
+            return footballplayerManger.GetAll(substring);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<FootballPlayer> Get(int id)
+        {
+            FootballPlayer player = footballplayerManger.GetById(id);
+            if (player == null)
+            {
+                return NotFound("No such item, id: " + id);
+            }
+            else
+            {
+                return Ok(player);
+            }
+
+        }
+
+        [HttpPost]
+        public FootballPlayer Post([FromBody] FootballPlayer value)
+        {
+            return footballplayerManger.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public FootballPlayer Put(int id, [FromBody] FootballPlayer value)
+        {
+            return footballplayerManger.Update(id, value);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public FootballPlayer Delete(int id)
+        {
+            return footballplayerManger.Delete(id);
+        }
+    }
+}
---
+++ Opgave 4 Simple REST Service/Mangers/FootballplayerManger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Opgave_1_Csharp;
+
+namespace Opgave_4_Simple_REST_Service.Mangers
+{
+    public class FootballplayerManger
+    {
+        private static List<FootballPlayer> footballPlayers = new List<FootballPlayer>()
+        {
+            new FootballPlayer {Name = "Anders", Price = 300000, ShirtNumber = 10, Id = 1},
+            new FootballPlayer {Name = "Benjamin", Price = 200000, ShirtNumber = 11, Id = 2},
+            new FootballPlayer {Name = "Morten", Price = 500000, ShirtNumber = 12, Id = 3}
+        };
+
+        public List<FootballPlayer> GetAll(string substring)
+        {
+            List<FootballPlayer> result = new List<FootballPlayer>();
+            if(substring != null)
+            {
+                result = result.FindAll(footballPlayers => footballPlayers.Name.Contains(substring, StringComparison.OrdinalIgnoreCase));
+            }
+            return result;
+        }
+		public FootballPlayer GetById(int id)
+		{
+			return footballPlayers.Find(item => item.Id == id);
+		}
+
+		public FootballPlayer Add(FootballPlayer newPlayer)
+		{
+			newPlayer.Id++;
+			footballPlayers.Add(newPlayer);
+			return newPlayer;
+		}
+
+		public FootballPlayer Delete(int id)
+		{
+			FootballPlayer player = footballPlayers.Find(player1 => player1.Id == id);
+			if (player == null) return null;
+			footballPlayers.Remove(player);
+			return player;
+		}
+
+		public FootballPlayer Update(int id, FootballPlayer updates)
+		{
+			FootballPlayer player = footballPlayers.Find(player1 => player1.Id == id);
+			if (player == null) return null;
+			player.Name = updates.Name;
+			player.Price = updates.Price;
+			player.ShirtNumber = updates.ShirtNumber;
+			return player;
+		}
+	}
+}
